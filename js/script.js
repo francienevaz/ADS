12fe110a265f772eeabbd5ad5a633231db9d0f7b
francienevaz <fran.vfraga@gmail.com>
2023-09-06 20:41:39 -0300

    updated

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    btn.innerHTML = "<h1>Quebrei</h1>";
+    btn.style.background = "rgb(226, 67, 67)";
+    // btn.style.padding = "20px"
+})
