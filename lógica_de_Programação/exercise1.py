0cdc69f37176c0a02fa543a680efbc50ca5b1bb0
francienevaz <fran.vfraga@gmail.com>
2023-09-16 00:01:10 -0300

    updated

--- lógica_de_Programação/exercise1.py
+++ lógica_de_Programação/exercise1.py
@@ -2,9 +2,11 @@ ladoI = int(input("Digite um número: "))
 ladoII = int(input("Digite outro número: "))
 ladoIII = int(input("Digite mais um número: "))
 
-if ladoI == ladoII == ladoIII:
-    print("\nOs três lados são iguais, logo o triângulo é um Equilátero")
-elif ladoI == ladoII or ladoI == ladoIII or ladoII == ladoIII:
-    print ("\nDois dos lados são iguais, logo o triâgulo é um Isósceles.")
-elif ladoI != ladoII != ladoIII:
-    print('\nTodos os lados são diferentes, logo o triângulo é Escaleno.')
+if ladoI and ladoII and ladoIII > 0:
+    if (ladoI + ladoII) > ladoIII and (ladoII + ladoIII) > ladoI and (ladoI + ladoIII) > ladoII:
+        if ladoI == ladoII == ladoIII:
+            print("\nOs três lados são iguais, logo o triângulo é um Equilátero")
+        elif ladoI == ladoII or ladoI == ladoIII or ladoII == ladoIII:
+            print ("\nDois dos lados são iguais, logo o triâgulo é um Isósceles.")
+        elif ladoI != ladoII != ladoIII:
+            print('\nTodos os lados são diferentes, logo o triângulo é Escaleno.')
