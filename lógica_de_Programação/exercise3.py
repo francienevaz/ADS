9c2080058d1f052390483157754721629c7adbc2
francienevaz <fran.vfraga@gmail.com>
2023-09-17 11:40:15 -0300

    updated

--- lógica_de_Programação/exercise3.py
+++ lógica_de_Programação/exercise3.py
@@ -11,7 +11,7 @@ if tipo_instalacao == 1:
 elif tipo_instalacao == 2:
     if kwh <= 100:
         taxa_comercial = 0.55
-    elif kwh > 500:
+    elif kwh > 100:
         taxa_comercial = 0.60
     print ('\nA sua tarifa é R${:.2f}'.format ((taxa_comercial* kwh)))
 elif tipo_instalacao == 3:
