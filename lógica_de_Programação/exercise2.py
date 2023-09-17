2445a1fe41e315d54de55f938e320c427e2a6175
francienevaz <fran.vfraga@gmail.com>
2023-09-17 11:36:55 -0300

    updated

--- lógica_de_Programação/exercise2.py
+++ lógica_de_Programação/exercise2.py
@@ -1,17 +1,18 @@
+msg = print('Calculadora Simples com Python')
+calc = input("Escolha uma operação: \n [01] - adição \n [02] - subtração \n [03] - multiplicação \n [04] - divisão : ")
 valorI = int(input("Digite um número: "))
 valorII = int(input("Digite outro número: "))
-calc = input("Escolha uma operação: \n adição \n subtração \n multiplicação \n divisão : ")
 
 soma = valorI + valorII
 subtracao = valorI - valorII
 multiplicacao = valorI * valorII 
 divisao = valorI / valorII
 
-if calc == "adição":
-    print(soma)
-elif calc == "subtração":
-    print(subtracao)
-elif calc == "multiplicação":
-    print(multiplicacao)
-elif calc == "divisão":
-    print(divisao)
+if calc == "01":
+    print(f'{valorI} + {valorII} = {soma}')
+elif calc == "02":
+    print(f'{valorI} - {valorII} = {subtracao}')
+elif calc == "03":
+    print(f'{valorI} * {valorII} = {multiplicacao}')
+elif calc == "04":
+    print(f'{valorI} / {valorII} = {divisao}')
---
+++ lógica_de_Programação/exercise3.py
@@ -0,0 +1,22 @@
+msg = print('Calculo do consumo de energia em kwh')
+tipo_instalacao = int(input('Escolha uma opção: \n [1] - Residência; \n [2] - Comercial; \n [3] - Indutrial: \n'))
+kwh = float(input('Digite o seu consumo em kwh: '));
+
+if tipo_instalacao == 1:
+    if kwh <= 500:
+        taxa_residencia = 0.40
+    elif kwh > 500:
+        taxa_residencia = 0.65
+    print('\nA sua tarifa é R${:.2f}'.format((taxa_residencia * kwh)))
+elif tipo_instalacao == 2:
+    if kwh <= 100:
+        taxa_comercial = 0.55
+    elif kwh > 500:
+        taxa_comercial = 0.60
+    print ('\nA sua tarifa é R${:.2f}'.format ((taxa_comercial* kwh)))
+elif tipo_instalacao == 3:
+    if kwh <= 5000:
+        taxa_industrial = 0.55
+    elif kwh > 5000:
+        taxa_industrial = 0.60
+    print ('A sua tarifa é R${:.2f}'.format (taxa_industrial * kwh))
