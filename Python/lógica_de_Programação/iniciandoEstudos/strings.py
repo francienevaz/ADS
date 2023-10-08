90ca85cadc07ef46bdae97a053bfedde4fa4905c
francienevaz <fran.vfraga@gmail.com>
2023-10-07 21:16:38 -0300

    updated

--- Python/lógica_de_Programação/iniciandoEstudos/strings.py
+++ Python/lógica_de_Programação/iniciandoEstudos/strings.py
@@ -1,6 +1,10 @@
 # Conhecendo métodos úteis da classe string
 
 curso = "pYtHon"
+teste = "$&*%#\@1\;^][)(]"
+teste2 = """dfkmkdflmkl
+            sdlkmkl
+            ]lsdkdkm """
 
 print(curso.upper())
 print(curso.lower())
@@ -22,4 +26,14 @@ print("-".join(hello))
 for letra in hello:
     print(letra, end="-")
 
+
+print(type(curso))
+print(type(teste))
+print(type(teste2))
+
+nome12345 = "Joao"
+área = "squad"
+_y_ = "é uma variável"
+print(_y_)
+print(type(_y_))
 
