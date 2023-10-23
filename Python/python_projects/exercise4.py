b5cb0bafbf511f80942fe06548300e606e097fcb
francienevaz <fran.vfraga@gmail.com>
2023-10-22 23:48:40 -0300

    updated

--- Python/python_projects/exercise4.py
+++ Python/python_projects/exercise4.py
@@ -1,9 +1,13 @@
 def main():
-    print("Seja Bem Vindo ao Controle de Livros da Franciene Vaz")
+    print("----- Seja Bem Vindo ao Controle de Livros da Franciene Vaz -----")
+    #Iniciando as variáveis
     lista_livros = []
     id_global = 0
 
+    #Definindo as funções
     def cadastrar_livro(id):
+        print("------------------- Menu Cadastrar Livros -------------------")
+        print('Id do livro {}'.format(id))
         livro = input("Por favor digite o nome do livro: ")
         autor = input("Por favor digite o nome do autor: ")
         editora = input("Por favor digite o nome da editora: ")
@@ -12,6 +16,7 @@ def main():
         lista_livros.append(dicionario)
 
     def consultar_livro():
+        print("------------------- Menu Consultar Livros -------------------")
         consulta = input(''' 
             Escolha a opção desejada:
             1 - Consultar todos
@@ -28,14 +33,20 @@ def main():
             elif consulta == '2':
                 id_consulta = int(input("Digite o ID do livro que deseja consultar: "))
                 print(lista_livros[id_consulta])
-            elif consulta == '3':    
+            elif consulta == '3':
+                #Solicita o nome do autor a ser consultado  
                 autor_consulta = input("Digite o nome do autor que deseja consultar: ")
+
+                #Para cada livro em lista_livros se o nome do autor for igual ao recebido em autor_consulta, livros_autor recebe os dados do livro
                 livros_autor = [livro for livro in lista_livros if livro['autor'] == autor_consulta]
+
+                #imprime a lista
                 print(livros_autor)
             else:
                 return
 
     def remover_livro():
+        print("--------------------- Menu Remover Livros ---------------------")
         remove_id = int(input("Por favor digite o ID do livro que deseja excluir: "))
         for i, livro in enumerate(lista_livros):
             if livro['id'] == remove_id:
@@ -43,6 +54,7 @@ def main():
                 break
 
     while True:
+        print("----------------------- Menu Principal -----------------------")
         opcao = input('''
             Digite a opção desejada:
             1 - Cadastrar livro
@@ -66,4 +78,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
