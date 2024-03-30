65f4adac81c379d0fadb5977e91f9beadf1df82e
francienevaz <fran.vfraga@gmail.com>
2024-03-30 13:01:40 -0300

    updated

--- js/script.js
+++ js/script.js
@@ -20,3 +20,50 @@ btn.addEventListener("click", () => {
 //     console.log(x)
 //     x--
 // }
+
+let exeObj = {
+
+
+    nr: 500,
+  
+  
+    str: "palavra"
+  
+  
+  };
+  
+console.log(exeObj.nr);
+console.log(exeObj.str); 
+
+let Teste = 'teste';
+let teste = 'outro teste';
+let TeSte = 'terceiro teste';
+
+console.log(Teste, teste, TeSte);
+
+let array = ['Lucas', 'Marcos', 'Pedro', 'Patricia'];
+
+console.log(array.indexOf('Patricia'));
+
+let nome1= 'Anna';
+
+
+{
+
+
+    let idade1=25;
+
+
+    console.log(nome1);
+
+
+    console.log(idade1);
+
+
+}
+
+
+console.log(nome1);
+
+
+console.log(idade1);
