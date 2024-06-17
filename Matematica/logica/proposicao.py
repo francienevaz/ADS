8af4681224d97dc5804fcbbb900c32173c6599ca
francienevaz <fran.vfraga@gmail.com>
2024-06-17 20:47:17 -0300

    updated

---
+++ Matematica/logica/proposicao.py
@@ -0,0 +1,27 @@
+# proposição é o conceito de 
+# uma afirmação que pode ser verdadeira ou falsa
+# em Python, uma proposição é representada por um valor booleano
+# que pode ser True (verdadeiro) ou False (falso)
+#
+# Operadores Lógicos
+#
+# Os operadores lógicos são utilizados para combinar proposições
+# e produzir um resultado booleano
+#
+# Exemplos de operadores lógicos:
+#
+# - and (e)
+# - or (ou)
+# - not (não)
+#
+# Exemplos de uso:
+#
+# - a and b: True se a e b forem True
+# - a or b: True se a ou b forem True
+# - not a: True se a for False
+
+M = float(input('Média obtida: '))
+if M < 70:
+    print("Você não passou!")
+else:
+    print("Você passou!")
