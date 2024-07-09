b02395b4199063cb50d96b89ce3e47bb29c6d112
francienevaz <fran.vfraga@gmail.com>
2024-07-09 18:35:58 -0300

    updated

---
+++ Matematica/APOLs/exercise.py
@@ -0,0 +1,43 @@
+# Qual é a forma hexadecimal associada ao número decimal 9192?
+# 16**0 = 1
+# 16**1 = 16
+# 16**2 = 256
+# 16**3 = 4096
+# 16**4 = 65536
+# 16**5 = 1048576
+# 16**6 = 16777216
+#
+# 9192 = a3.16**3 + a2.16**2 + a1.16**1 + a0.16**0
+# a3 = 9192/4096 = 2,... -> pegamos a parte inteira para representar a3, e segue assim sucessivamente
+# 9192 =  2*4096 + a2.16**2 + a1.16**1 + a0.16**0
+# 9192 = 8192 + a2.16**2 + a1.16**1 + a0.16**0 ->> 9192-8192 = 1000
+# 1000 = a2.16**2 + a1.16**1 + a0.16**0
+# a2 = 1000/256 = 3,... -> pegamos a parte inteira
+# 1000 = 3*256 + a1.16**1 + a0.
+# 1000 = 768 + a1.16**1 + a0.16**0
+# a1 = 1000-768 = 232
+# a1 = 232/16 = 14,... -> pegamos a parte inteira
+# 232 = 14*16 + a0.16**0
+# a0 = 232-14*16 = 8
+# a0 = 8/1 = 8,... -> pegamos a parte inteira
+# 9192 = 2*4096 + 3*256 + 14*16 = 23E8
+# o hexadecimal equivale aos a0, a1, a2, e a3 = 23e8
+
+# Dados a=8,151521 x 10^3 e b=2,456732 x 10^3, calcule a.b.
+
+# Escreva o número decimal 89 na respectiva forma binária.
+#  89%2 = 1 44%2 0 22%2 0 11%2 1 5%2 1 2%2 0 1%2 1 = 1011001 da direita para a esquerda
+
+# Qual é a forma hexadecimal associada ao número decimal 12001?
+
+# Dados a=8,151521 x 103 e b=2,456732 x 103, calcule a+b.
+
+# Qual é a representação na forma binária referente ao decimal 57?
+
+# Qual é a forma decimal referente ao hexadecimal 919?
+
+# Represente na forma decimal o número hexadecimal C413.
+
+# Represente o número 12453301 utilizando a notação científica normalizada.
+
+# Dados a=8,151521 x 105 e b=2,456732 x 103, calcule a+b.
